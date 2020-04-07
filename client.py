2bc45e9501d2f08cc0d5cca610832efc64417420
als1379 <a.saleh.m.a@gmail.com>
2020-04-07 08:11:11 +0430

    add send

--- client.py
+++ client.py
@@ -6,18 +6,40 @@ class RedisClient:
     async def connect(self, host, port):
         self.r , self.w = await asyncio.open_connection(host, port)
 
-    async def set(self, key, value):
-        self.w.write(f"SET {key} {value} \r\n".encode())
-        await self.w.drain()
-        return await self._read_reply()
-
-    async def get(self, key):
-        self.w.write(f"GET {key} \r\n".encode())
+    async def send(self, *args):
+        resp_args = "".join([f"${len(x)}\r\n{x}\r\n" for x in args])
+        self.w.write(f"*{len(args)}\r\n{resp_args}".encode())
         await self.w.drain()
         return await self._read_reply()
 
     async def _read_reply(self):
         tag = await self.r.read(1)
+
+        if tag == b'$':
+            length = b''
+            ch = b''
+            while ch != b'\n':
+                ch = await self.r.read(1)
+                length += ch
+            length = int(length[:-1]) + 2
+            result = await self.r.read(length)
+            return result[:-2].decode()
+
+        if tag == b':':
+            result = b''
+            ch = b''
+            while ch != b'\n':
+                ch = await  self.r.read(1)
+                result += ch
+            return int(result[:-1].decode())
+
+        if tag == b'-':
+            result = b''
+            ch = b''
+            while ch != b'\n':
+                ch = await  self.r.read(1)
+                result += ch
+            raise Exception(result[:-1].decode())
 
         if tag == b'+':
             result = b''
@@ -26,6 +48,20 @@ class RedisClient:
                 ch = await  self.r.read(1)
                 result += ch
             return result[:-1].decode()
+
+        if tag == b'*':
+            len = int(await self.r.read(1))
+            enter = await self.r.read(2)
+            result = []
+            for _ in range(len):
+                dollar = await self.r.read(1)
+                key_len = int(await self.r.read(1))
+                enter = await self.r.read(2)
+                key = await self.r.read(key_len)
+                result.append(key.decode())
+                enter = await self.r.read(2)
+
+            return result
         else:
             msg = await self.r.read(100)
             raise Exception(f"Unkown tag: {tag}, msg: {msg}")
@@ -34,9 +70,12 @@ class RedisClient:
 async def start():
     client = RedisClient()
     await client.connect("localhost", 6379)
-    print(await client.set("ali", "fuck"))
-    print(await client.get("ali"))
-
+    query = ""
+    while query != "quit":
+        query = input("enter query: ").split()
+        if query[0] == "quit":
+            break
+        print(await client.send(*query))
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
